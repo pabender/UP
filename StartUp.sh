f20a973fcb600d02c6d3c825a19f306374f9e7b9
Paul Bender <paul.bender@acm.org>
2019-11-16 14:57:15 -0500

    Fix IP address

--- StartUp.sh
+++ StartUp.sh
@@ -5,7 +5,7 @@ unset https_proxy
 unset ftp_proxy
 
 # Add an alias ip address to make sure the server can be found.
-sudo ip address add 192.168.2.99/24 dev eth0
+sudo ip address add 192.168.1.99/24 dev eth0
 
 #check for updates
 cd /home/pi
