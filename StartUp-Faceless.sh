0f4e955b4bafb0eb6f5a23278e000d84cd1e5d54
Paul Bender <paul.bender@acm.org>
2019-11-16 22:18:40 -0500

    update IP address

--- StartUp-Faceless.sh
+++ StartUp-Faceless.sh
@@ -4,7 +4,7 @@ unset http_proxy
 unset ftp_proxy
 
 # Add an alias ip address to make sure the server can be found.
-sudo ip address add 192.168.2.99/24 dev eth0
+sudo ip address add 192.168.1.99/24 dev eth0
 
 #check for updates
 cd /home/pi
