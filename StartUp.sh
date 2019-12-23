400b46fd69f604facabf0302055ca0d4a021d6ce
Paul Bender <paul.bender@acm.org>
2019-12-23 00:19:29 -0600

    update to include CATS locally

--- StartUp.sh
+++ StartUp.sh
@@ -7,6 +7,13 @@ unset ftp_proxy
 # Add an alias ip address to make sure the server can be found.
 sudo ip address add 192.168.1.99/24 dev eth0
 
+#get cats
+cd /home/pi
+wget http://cats4ctc.wikidot.com/local--files/main:downloads/release2043.zip
+unzip release2043.zip
+cp release/* JMRI
+chmod +x JMRI/cats.csh
+
 # update java
 #cd /home/pi
 #./updatejava.sh
@@ -18,4 +25,5 @@ git pull
 #start panelpro
 
 cd /home/pi/JMRI
-./PanelPro
+#./PanelPro
+./cats.csh
