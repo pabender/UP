97ce599b0d72a33280b2d5a197c095c4e5b6b1f3
Paul Bender <paul.bender@acm.org>
2020-01-23 23:33:56 -0600

    do a full upgrade once

--- updatejmri.sh
+++ updatejmri.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [! -e updateonce ]
+	touch updateonce
+	sudo apt-get update
+	sudo apt-get fullupgrade
+	sudo shutdown -rf now
+fi
+
+
 if [ -e JMRI.4.19.2+R846b684.tgz ]
 else
 	wget https://github.com/JMRI/JMRI/releases/download/v4.19.2/JMRI.4.19.2+R846b684.tgz
