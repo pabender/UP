22779a78e43bbc675546caf7372a8aa95f3fedfd
Paul Bender <paul.bender@acm.org>
2020-01-24 00:05:19 -0600

    fix update

--- update.sh
+++ update.sh
@@ -10,16 +10,6 @@ git pull
 cd /home/pi
 ./updatejmri.sh
 
-#get cats
-cd /home/pi
-wget http://cats4ctc.wikidot.com/local--files/main:downloads/release2043.zip
-unzip release2043.zip
-cp release/* JMRI
-chmod +x JMRI/cats.csh
-cd /home/pi
-rm -r release
-rm release2043.zip
-
 # update java
 #cd /home/pi
 #./updatejava.sh
--- updatejmri.sh
+++ updatejmri.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-if [! -e updateonce ]
-	touch updateonce
+if [ -e updateonce ]
+then
+	echo ""
+else
+	echo "" > updateonce
 	sudo apt-get update
 	sudo apt-get fullupgrade
 	sudo shutdown -rf now
@@ -9,9 +12,22 @@ fi
 
 
 if [ -e JMRI.4.19.2+R846b684.tgz ]
+then
+	echo ""
 else
 	wget https://github.com/JMRI/JMRI/releases/download/v4.19.2/JMRI.4.19.2+R846b684.tgz
 	rm -rf JMRI
 	tar xvzf JMRI.4.19.2+R846b684.tgz
+
+	#get cats
+        cd /home/pi
+        wget http://cats4ctc.wikidot.com/local--files/main:downloads/release2043.zip
+        unzip release2043.zip
+        cp release/* JMRI
+        chmod +x JMRI/cats.csh
+        cd /home/pi
+        rm -r release
+        rm release2043.zip
 fi
+
 
