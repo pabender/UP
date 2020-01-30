686335a77ce07b9e17514a3b10394643ad6558cb
Paul Bender <paul.bender@acm.org>
2020-01-29 21:03:46 -0600

    setup vnc

--- StartHeadedApp.sh
+++ StartHeadedApp.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-curl https://www.dataplicity.com/fz6048no.py | sudo python
+#curl https://www.dataplicity.com/fz6048no.py | sudo python
+sudo ln -s /usr/lib/systemd/system/vncserver-x11-serviced.service /etc/systemd/system/multi-user.target.wants/vncserver-x11-serviced.service
 
 cd /home/pi/JMRI
 ./PanelPro
